0cca8a3866766ebb14b29ecbe4234092322eb20f
step network <snetgh@gmail.com>
2019-04-07 16:36:33 +0100

    change of email address at email backend

--- member/settings.py
+++ member/settings.py
@@ -145,8 +145,8 @@ LOGIN_URL = '/client/login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'holyspiritmovements@gmail.com'
-EMAIL_HOST_PASSWORD = 'jyiqjgkjrjfizmea'
+EMAIL_HOST_USER = 'snetghmember@gmail.com'
+EMAIL_HOST_PASSWORD = 'wtfwffgzzutvszqi'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
